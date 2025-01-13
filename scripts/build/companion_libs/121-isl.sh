646bfb6e90a23c370df407a911a031a794d021be
Hari Santoso <hari@optimasi.ai>
2025-01-13 13:31:35 +0700

    add log info in isl

--- scripts/build/companion_libs/121-isl.sh
+++ scripts/build/companion_libs/121-isl.sh
@@ -13,6 +13,7 @@ if [ "${CT_ISL}" = "y" ]; then
 
 # Download ISL
 do_isl_get() {
+    CT_DoStep INFO "target url https://gcc.gnu.org/pub/gcc/infrastructure"
     CT_GetFile "isl-${CT_ISL_VERSION}" \
         https://gcc.gnu.org/pub/gcc/infrastructure
 }
